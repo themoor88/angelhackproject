0e507835fa737a41aeae3d13c8275fa31ae291b5
Wajid Kagzi <wjkagzi@gmail.com>
2015-06-13 14:47:54 -0400

    Adding seeds to the NoSQL database.

---
+++ server/seeder.js
@@ -0,0 +1,14 @@
+Meteor.startup(function(){
+  Factory.define('message', Messages, {
+    text: function() {
+      return Fake.sentence();
+    }
+  });
+
+  Messages.remove({});
+
+  if (Messages.find({}).count() === 0) {
+    _(10).times
+  }
+
+});

b50ba3f6e4cc2d94c6e54820c160035d1a8260da
Wajid Kagzi <wjkagzi@gmail.com>
2015-06-13 14:10:28 -0400

    Initial part of building a slack clone from meteor.

---
+++ client/app.js
@@ -0,0 +1,10 @@
+Template.messages.helpers({
+  messages: [
+    { text: "All these messages" },
+    { text: "are the same" },
+    { text: "thing" },
+    { text: "It's why these messages are the same" },
+    { text: "Cool beans" },
+    { text: "Check it out" }
+  ]
+});

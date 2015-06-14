d0bd0b09e4956b5c5ae828d76442f957a02a6221
Wajid Kagzi <wjkagzi@gmail.com>
2015-06-14 12:57:39 -0400

    Final changes for Hackathon

--- server/seeder.js
+++ server/seeder.js
@@ -1,34 +1,21 @@
-// Meteor.startup(function() {
+Meteor.startup(function() {
 
-//   Meteor.users.remove({});
-//   Accounts.createUser({
-//     username: "tupacmo",
-//     email: "wjkagzi@gmail.com",
-//     password: "password"
-//   });
+  Meteor.users.remove({});
+  Accounts.createUser({
+    username: "tupacmo",
+    email: "wjkagzi@gmail.com",
+    password: "password"
+  });
 
-//   Factory.define('message', Messages, {
-//     text: function() {
-//       return Fake.sentence();
-//     },
-//     user: Meteor.users.findOne()._id,
-//     timestamp: Date.now(),
-//     channel: 'general'
-//   });
 
-//   Messages.remove({});
 
-//   if (Messages.find({}).count() === 0) {
-//     _(5).times(function(n) {
-//       Factory.create('message');
-//     });
-//   }
+  Messages.remove({});
 
-//   Channels.remove({});
-//   Channels.insert({
-//     name: "general"
-//   });
-//   Channels.insert({
-//     name: "random"
-//   });
-// });
+  Channels.remove({});
+  Channels.insert({
+    name: "general"
+  });
+  Channels.insert({
+    name: "random"
+  });
+});
